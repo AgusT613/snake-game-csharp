5e24ff8bf88eb4f0f00392a2b225183fedece9c6
Agus Torres <torresagustin613@gmail.com>
2025-02-10 19:51:41 -0300

    test: add xunit test for ScreenGame class

---
+++ SnakeGame.Tests/SnakeGameShould.cs
@@ -0,0 +1,37 @@
+﻿namespace SnakeGame.Tests;
+
+public class SnakeGameShould
+{
+    [Theory]
+    [InlineData(10, 10)]
+    [InlineData(15, 10)]
+    public void DisplayScreen(int columns, int rows)
+    {
+        var screenObj = new ScreenGame(columns: columns, rows: rows);
+        string[,] screen = ScreenGame.InitScreen(screenObj.ColumnLength, screenObj.RowLength, screenObj.ScreenBackgroundIcon);
+
+        var columnLength = screen.GetLength(1);
+        var rowLength = screen.GetLength(0);
+
+        Assert.True(columnLength == columns);
+        Assert.True(rowLength == rows);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(0, 9)]
+    [InlineData(14, 9)]
+    public void InsertOneElementToScreen(int column, int row)
+    {
+        var screenObj = new ScreenGame(columns: 15, rows: 10);
+        string[,] screen = ScreenGame.InitScreen(screenObj.ColumnLength, screenObj.RowLength, screenObj.ScreenBackgroundIcon);
+        int[] newPosition = [column, row];
+        string newIcon = "@";
+
+        ScreenGame.Insert(newPosition, screen, newIcon);
+
+        Assert.True(screen[row, column] == newIcon);
+    }
+
+
+}
